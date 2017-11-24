12a21c475b705d409f547244a4bda03c8a557dd2
Alberto Pino <33347576+AlbertoPino@users.noreply.github.com>
2017-11-24 21:24:02 +0100

    Update codigo.pl
    Arreglado el funcionamiento de GitHub, falta implementar que asigne el numero a dia de la semana correspondiente del Array

--- .gitignore/codigo.pl
+++ .gitignore/codigo.pl
@@ -2,19 +2,21 @@
 
 print "Content-Type: text/html\n\n";
 
-@days = ("Lunes" "Martes" "Miercoles" "Jueves" "Viernes" "Sabado" "Domingo");
 
 
 if($ENV{'QUERY_STRING'} eq "") {
 print "<h1> Dime un numero y veamos que sale<h1>
-	<form method=\"get\" >";
+        <form method=\"get\" >";
+@days = ("Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo");
 
 print "<input type=\"text\"  name=\"numero\"  />";
 print "<input type=\"submit\" value=\"Search\" />";
 
-}else{  
+}else{
 
         @days=split("=",$ENV{'QUERY_STRING'});
-	print "$days[1]\n";
+        print "$days[1]\n";
 
 }
+
+
